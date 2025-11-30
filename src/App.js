2bd7e941ab0abd5d222f5f4250d3063654b373d9
chitizkr <44926057+chitizkr@users.noreply.github.com>
2025-11-30 17:04:15 +0530

    undo/redu logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import UndoRedu from "./projects/undoRedu/UndoRedu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UndoRedu />
     </div>
   );
 }
---
+++ src/projects/undoRedu/UndoRedu.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./UndoRedu.css";
+
+const initialValue = 0;
+
+function UndoRedu() {
+  const [past, setPast] = useState([]);
+  const [present, setPresent] = useState(initialValue);
+  const [future, setFuture] = useState([]);
+
+  function set(data) {
+    setPast([...past, present]);
+    setPresent((e) => e + data);
+  }
+
+  function undo() {
+    if (past.length === 0) return;
+    const prev = past[past.length - 1];
+    const presentC = past.slice(0, past.length - 1);
+
+    setFuture((el) => [present, ...el]);
+    setPresent(prev);
+    setPast(presentC);
+  }
+
+  function redo() {
+    // alert("re");
+    if (future.length === 0) return;
+    const next = future[0];
+    const newfuture = future.slice(1);
+
+    setPresent(next);
+    setPast([...past, next]);
+    setFuture(newfuture);
+  }
+
+  return (
+    <div>
+      <h1>Undoable Counter</h1>
+      <div className="button">
+        <button className="btn" onClick={undo}>
+          Undo
+        </button>
+        <button className="btn" onClick={redo}>
+          Redo
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setPast([]);
+            setPresent(initialValue);
+            setFuture([]);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="body">
+        <button className="btn-c" onClick={() => set(-1)}>
+          -1
+        </button>
+        <button className="btn-c" onClick={() => set(-10)}>
+          -10
+        </button>
+        <button className="btn-c" onClick={() => set(-100)}>
+          -100
+        </button>
+        <h2>{present}</h2>
+        <button className="btn-c" onClick={() => set(1)}>
+          +1
+        </button>
+        <button className="btn-c" onClick={() => set(10)}>
+          +10
+        </button>
+        <button className="btn-c" onClick={() => set(100)}>
+          +100
+        </button>
+      </div>
+      <div>
+        <h3>History</h3>
+      </div>
+    </div>
+  );
+}
+export default UndoRedu;
